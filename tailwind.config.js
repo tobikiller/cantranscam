3281b829256dc57e5ca85a9d2b13008c2994d174
tobikiller <edwardnguimeya@yahoo.com>
2022-07-08 03:00:30 +0100

    new

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./index.html','./index.js',],
+  theme: {
+    extend: {
+
+      colors:{
+        "Deepblue":"#03045E",
+        "LightBlue":"#135DFF"
+      },
+      fontFamily:{
+        "Quantico":"Quantico",
+        "Poppins": "Poppins"
+      },
+      backgroundImage:{
+        "world-map":"url('images/world map.png') "
+      }
+
+    },
+  },
+  plugins: [],
+}
