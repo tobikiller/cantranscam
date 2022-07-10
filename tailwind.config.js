25bc5dfaca5a3266cc82ebb5d3ce5aa97a70f615
tobikiller <edwardnguimeya@yahoo.com>
2022-07-10 18:33:24 +0100

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,10 @@ module.exports = {
         "Poppins": "Poppins"
       },
       backgroundImage:{
-        "world-map":"url('images/world map.png') "
+        "world-map":"url('images/world map.png') ",
+        "profile-1":"url('images/profile1.png') ",
+        "profile-2":"url('images/profile2.png') ",
+        "profile-3":"url('images/profile3.png') ", 
       }
 
     },
