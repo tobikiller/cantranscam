d973353bf2244b9024ea0aba098c5a9660f23619
tobikiller <edwardnguimeya@yahoo.com>
2022-07-22 01:03:14 +0100

    new

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ gsap.registerPlugin(ScrollTrigger)
 let tl = gsap.timeline({default: {ease:"SlowMo.easeOut"}})
 
 tl.from(".small-title",{
-    scrollTrigger: ".small-title",
+    
     y:20,
     opacity:0,
     duration:1,
@@ -52,7 +52,36 @@ tl.from(".small-text",{
     y:20,
     opacity:0,
     duration:1,
-  
-  
+})
 
+
+gsap.from(".about-title",{
+    scrollTrigger: {
+        trigger:".about-title",
+        toggleActions:"play none none none"
+        
+    },
+
+    y:20,
+    opacity:0,
+    duration:1,
+    ease:"power1.out",
+    stagger:0.5
+})
+
+
+
+TweenMax.from(".mask-text",{
+    scrollTrigger: {
+        trigger:".mask-text",
+        start:"top center",
+        markers:"true",
+        toggleActions:"play none none none"
+        
+    },
+    y:40,
+    opacity:0,
+   duration:1.5,
+ 
+   ease:"power3.out",
 })
